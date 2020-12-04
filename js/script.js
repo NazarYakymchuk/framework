984b7b3169ec3b2788e431b02fcf2964167b4b82
NazarYakymchuk <50630986+NazarYakymchuk@users.noreply.github.com>
2020-12-04 17:18:11 +0200

    s_0009

--- js/script.js
+++ js/script.js
@@ -5,3 +5,31 @@ hamburger.addEventListener('click', () => {
     nav.classList.toggle('active');
     hamburger.classList.toggle('active');
 });
+
+const hamburger_01 = document.getElementById('hamburge_01');
+const nav_01 = document.getElementById('nav-hamburger_01');
+
+hamburger_01.addEventListener('click', () => {
+    nav_01.classList.toggle('active');
+    hamburger_01.classList.toggle('active');
+});
+
+
+new Swiper('.sl_slider', {
+    effect: 'cube',
+    grabCursor: true,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
